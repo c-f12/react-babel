5b05692b0ee5688d28b814f2872d7fe823ae19e1
Rafa Ramírez <raffe90@gmail.com>
2018-04-12 00:06:40 +0200

    Stateless handling

---
+++ src/components/Intro/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            message: props.message
+        }
+    }
+
+    render() {
+        const { message } = this.state
+
+        return (
+            // <p className="App-intro" dangerouslySetInnerHTML={{ __html: message }} />
+            <p className="App-intro">
+                {message}
+            </p>
+        );
+    }
+}
+
+export default Intro;
